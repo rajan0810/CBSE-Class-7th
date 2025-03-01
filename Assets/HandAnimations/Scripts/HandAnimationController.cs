6d8bc4d2ddb67ed60d0eb7fd913ff9fd5059e0a4
GoodKarma27 <98578146+IshanKhambhaliya@users.noreply.github.com>
2025-03-01 22:36:20 +0530

    Ath3na Saved !

--- Assets/HandAnimations/Scripts/HandAnimationController.cs
+++ Assets/HandAnimations/Scripts/HandAnimationController.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class HandAnimationController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Animator leftHandAnimator;
+    public Animator rightHandAnimator;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Update animations for both hands
+        UpdateHandAnimation(leftHandAnimator);
+        UpdateHandAnimation(rightHandAnimator);
+    }
+
+    void UpdateHandAnimation(Animator handAnimator)
+    {
+        bool isGripPressed = Input.GetButton("Grip");  // Replace with VR input later
+        bool isTriggerPressed = Input.GetButton("Trigger");
+
+        int handState = 0; // Default state: Normal Hand
+
+        if (isGripPressed && isTriggerPressed)
+        {
+            handState = 3; // Fist (Both Grip + Trigger)
+        }
+        else if (isTriggerPressed)
+        {
+            handState = 2; // Point (Only Trigger)
+        }
+        else if (isTriggerPressed && !isGripPressed)
+        {
+            handState = 1; // Pinch (Light Trigger Press)
+        }
+
+        handAnimator.SetInteger("HandState", handState);
     }
 }
